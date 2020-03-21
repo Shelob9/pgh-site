fec424326a6c001e2446b20b40d84a6f3f4aab5d
Josh Pollock <jpollock412@gmail.com>
2020-03-21 18:58:49 -0400

    Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,80 +5,34 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 const Contact = () => {
-  return (
-    <Layout>
-      <SEO title="Contact" />
+	return (
+		<Layout>
+			<SEO title="Contact" />
 
-      <div className="min-h-screen mb-6 flex flex-col items-start">
-        <div className="w-3/4 md:w-1/2 mt-24 font-serif font-hairline">
-          <h1 className="text-4xl md:text-5xl text-indigo-700">Contact</h1>
-        </div>
-        <div
-          className="w-full sm:w-3/4 lg:w-1/2 mt-10 px-6 py-4"
-          style={{
-            boxShadow:
-              "0 15px 35px rgba(50,50,93,.1), 0 5px 15px rgba(0,0,0,.07)"
-          }}
-        >
-          <p>
-            <strong>Phone:</strong> XXX XXX XXX
-          </p>
-          <p>
-            <strong>Email: </strong>xxx@xxx.com
-          </p>
-        </div>
-        <div className="w-full">
-          <h3 className="mt-10 text-2xl">Business Hours</h3>
-          <table className="w-full md:w-3/4 text-left w-full border-collapse">
-            <tbody>
-              <tr className="border-t h-10">
-                <td className="font-semibold">Monday</td>
-                <td>8:30am</td>
-                <td>-</td>
-                <td>5:00pm</td>
-              </tr>
-              <tr className="border-t h-10">
-                <td className="font-semibold">Tuesday</td>
-                <td>8:30am</td>
-                <td>-</td>
-                <td>5:00pm</td>
-              </tr>
-              <tr className="border-t h-10">
-                <td className="font-semibold">Wednesday</td>
-                <td>8:30am</td>
-                <td>-</td>
-                <td>5:00pm</td>
-              </tr>
-              <tr className="border-t h-10">
-                <td className="font-semibold">Thursday</td>
-                <td>8:30am</td>
-                <td>-</td>
-                <td>5:00pm</td>
-              </tr>
-              <tr className="border-t h-10">
-                <td className="font-semibold">Friday</td>
-                <td>8:30am</td>
-                <td>-</td>
-                <td>5:00pm</td>
-              </tr>
-              <tr className="border-t h-10">
-                <td className="font-semibold">Saturday</td>
-                <td>Closed</td>
-                <td />
-                <td />
-              </tr>
-              <tr className="border-t h-10">
-                <td className="font-semibold">Sunday</td>
-                <td>Closed</td>
-                <td />
-                <td />
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </Layout>
-  );
+			<div className="min-h-screen mb-6 flex flex-col items-start">
+				<div className="w-full mt-24 font-serif font-hairline">
+					<h1 className="text-4xl md:text-5xl text-black-700">Contact</h1>
+				</div>
+				<div
+					className="w-full  mt-10 px-6 py-4"
+					style={{
+						boxShadow:
+							"0 15px 35px rgba(50,50,93,.1), 0 5px 15px rgba(0,0,0,.07)"
+					}}
+				>
+					<p className="text-base">
+						This site was created by
+						<a href="https://joshpress.net">Josh Pollock</a>
+					</p>
+					<p className="text-base">
+						Please feel free to reach out via
+						<a href="https://twitter.com/josh412"> Twitter</a> or email
+						JPollock412 [at] gmail
+					</p>
+				</div>
+			</div>
+		</Layout>
+	);
 };
 
 export default Contact;
