9eec58f5ef629a71e5114cb159ffe8733767bea0
Josh Pollock <jpollock412@gmail.com>
2020-03-21 18:54:57 -0400

    Add stuff to front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const TestingSite = props => (
 		<div className="h-full m-2 p-4 border-2 border-gray-300 flex flex-col items-center text-center">
 			<p className="text-2xl w-full">
 				<Link
-					to={props.path}
+					path={props.path}
 					className="text-2xl text-black-700 hover:text-indogo-600 hover:underline"
 				>
 					{props.title}
@@ -41,56 +41,70 @@ function Index({ data }) {
 		<Layout headerClass="relative bg-white">
 			<SEO title="Home" />
 			<div className="min-h-screen pt-24 sm:pt-32 md:pt-64 -mt-12 sm:-mt-16 md:-mt-24 lg:-mt-28 mb-20 bg-size-5/6 md:bg-size-4/5 lg:bg-size-2/3 bg-right-top bg-no-repeat flex flex-col items-center">
-				<div className="w-4/5 md:w-3/4 lg:w-7/12 mt-20 font-serif font-hairline self-start">
-					<h1 className="text-3xl md:text-5xl text-black-700 leading-tight">
-						{title}
-					</h1>
-					<p className="text-base">{description}</p>
-				</div>
-				<div className="flex flex-wrap justify-center items-stretch -mx-2">
-					{data.allMarkdownRemark.edges.map(testingSite => (
-						<TestingSite
-							{...testingSite.node.frontmatter}
-							excerpt={testingSite.node.excerpt}
-						/>
-					))}
-				</div>
+				<section>
+					<div className=" mt-20 font-serif font-hairline self-start">
+						<h1 className="text-3xl md:text-5xl text-black-700 leading-tight">
+							{title}
+						</h1>
+						<p className="text-base">{description}</p>
+					</div>
+				</section>
 
-				<div className="flex flex-col mt-10 md:mt-16">
-					<p className="mb-2 text-4xl text-gray-800 self-center">
-						Our Features
-					</p>
-
+				<section>
+					<h2 className="mb-2 text-4xl text-gray-800 self-center">
+						Testing Sites
+					</h2>
 					<div className="flex flex-wrap justify-center items-stretch -mx-2">
-						<div className="w-full md:w-1/2 lg:w-1/3 mt-2">
-							<div className="h-full m-2 p-4 border-2 border-gray-300 flex flex-col items-center text-center">
-								<div className="w-16 h-16 mb-4">
-									<img alt="branding" src={branding} />
+						{data.allMarkdownRemark.edges.map(testingSite => (
+							<TestingSite
+								{...testingSite.node.frontmatter}
+								excerpt={testingSite.node.excerpt}
+							/>
+						))}
+					</div>{" "}
+				</section>
+				<section>
+					<div className="flex flex-col mt-10 md:mt-16">
+						<h2 className="mb-2 text-4xl text-gray-800 self-center">
+							More Resources
+						</h2>
+
+						<div className="flex flex-wrap justify-center items-stretch -mx-2">
+							<div className="w-full md:w-1/2 lg:w-1/3 mt-2">
+								<div className="h-full m-2 p-4 border-2 border-gray-300 flex flex-col items-center text-center">
+									<Link path={"/links"}>
+										<h3 className="text-2xl w-full">Links</h3>
+									</Link>
+
+									<p>
+										Information about COVID-19 for Pittsburgh, Allegheny County
+										and Pennsylvania.
+									</p>
 								</div>
-								<p className="text-2xl w-full">Free Consultation</p>
-								<p>New clients recieve an obligation free consultation.</p>
 							</div>
-						</div>
-						<div className="w-full md:w-1/2 lg:w-1/3 mt-2">
-							<div className="h-full m-2 p-4 border-2 border-gray-300 flex flex-col items-center text-center">
-								<div className="w-16 h-16 mb-4">
-									<img alt="processing" src={processing} />
+							<div className="w-full md:w-1/2 lg:w-1/3 mt-2">
+								<div className="h-full m-2 p-4 border-2 border-gray-300 flex flex-col items-center text-center">
+									<Link path={"/help"}>
+										<p className="text-2xl w-full">Help Others</p>
+									</Link>
+
+									<p>
+										Ways to help the most vulnerable in our community during at
+										this time.
+									</p>
 								</div>
-								<p className="text-2xl w-full">Certified Accountants</p>
-								<p>All members of our team are certified accountants.</p>
 							</div>
-						</div>
-						<div className="w-full md:w-1/2 lg:w-1/3 mt-2">
-							<div className="h-full m-2 p-4 border-2 border-gray-300 flex flex-col items-center text-center">
-								<div className="w-16 h-16 mb-4">
-									<img alt="modeling" src={modeling} />
+							<div className="w-full md:w-1/2 lg:w-1/3 mt-2">
+								<div className="h-full m-2 p-4 border-2 border-gray-300 flex flex-col items-center text-center">
+									<Link path={"/diy"}>
+										<p className="text-2xl w-full">DIY</p>
+									</Link>
+									<p>How to make your own masks, hand sanitzier and more.</p>
 								</div>
-								<p className="text-2xl w-full">Tax Compliance</p>
-								<p>We stay up to date on the latest changes to the tax code.</p>
 							</div>
 						</div>
 					</div>
-				</div>
+				</section>
 			</div>
 		</Layout>
 	);
